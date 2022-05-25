d488176f93472679c6e84dc9e08af0fed30b141b
Dhruv Kapur <37783178+dkapur17@users.noreply.github.com>
2022-05-26 00:49:04 +0530

    Added Server Connection Logic

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 const path = require('path');
 const express = require('express');
-const ip = require('ip');
+const os = require('os');
+const networkInterfaces = os.networkInterfaces();
+const apiRouter = require('./routes');
 
 const app = express();
 
+
 const PORT = process.env.PORT || 5000;
 
+app.use('/api', apiRouter);
+app.use(express.static(path.resolve(__dirname, '../client/out/')));
 
-app.use('/views', express.static(path.resolve(__dirname, './views')));
 
 app.listen(PORT, () => {
-    console.log(`Server Up on Port ${PORT}`);
-    console.log(`Local IP: ${ip.address()}`);
+    console.log(`Server Up on`);
+    Object.keys(networkInterfaces).forEach(device => {
+        address = networkInterfaces[device].filter(address => address.family === 'IPv4');
+        address.forEach(address => {
+            console.log(`http://${address.address}:${PORT} (${device})`);
+        });
+    });
 });
