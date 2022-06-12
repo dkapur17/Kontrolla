b2a0c10105eb4fd7e28a9fe268ebb5ca20df7690
Dhruv Kapur <dhruvkapur4@gmail.com>
2022-06-12 21:08:17 +0530

    Express Server works now

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,51 @@
 const express = require('express');
+const robot = require('robotjs');
+const { mouse } = require('@nut-tree/nut-js');
+const wsRouter = express.Router();
 
-const apiRouter = express.Router();
+wsRouter.get('/inputHandler', (req, res) => {
+    const io = req.app.get('socket.io');
 
-apiRouter.get('/inputHandler', (req, res) => {
-    console.log("Websocket connection request recieved");
+    if (!res.socket.server.io) {
+
+        io.on('connection', socket => {
+
+            socket.on('click', (args) => {
+                const { button, double } = args;
+                robot.mouseClick(button, double);
+            });
+            socket.on('mouseMove', (args) => {
+                const { xChange, yChange } = args;
+                const { x, y } = robot.getMousePos();
+                robot.moveMouse(x + xChange, y + yChange);
+            });
+            socket.on('mouseScroll', async (args) => {
+                const { xChange, yChange } = args;
+                mouse.scrollUp(yChange);
+                await mouse.scrollRight(xChange);
+            });
+            socket.on('keypress', (args) => {
+                const { key, shift } = args;
+                try {
+                    if (shift)
+                        robot.keyTap(key, 'shift')
+                    else
+                        robot.keyTap(key);
+                }
+                catch (e) {
+                    console.log(`Function not supported on ${process.platform}`);
+                }
+            });
+
+        });
+
+        res.socket.server.io = io;
+    }
+    else {
+        console.log("Socket Is already Running");
+    }
+
+    res.end();
 });
 
-module.exports = apiRouter;
+module.exports = wsRouter;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,14 @@
-const path = require('path');
 const express = require('express');
+const { Server } = require('socket.io');
+const path = require('path');
 const os = require('os');
 const networkInterfaces = os.networkInterfaces();
-const apiRouter = require('./routes');
+const wsRouter = require('./routes');
 
 const app = express();
-
-
 const PORT = process.env.PORT || 5000;
 
-app.use('/api', apiRouter);
-app.use(express.static(path.resolve(__dirname, '../client/out/')));
-
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server Up on`);
     Object.keys(networkInterfaces).forEach(device => {
         address = networkInterfaces[device].filter(address => address.family === 'IPv4');
@@ -22,3 +17,17 @@ app.listen(PORT, () => {
         });
     });
 });
+
+const io = new Server(server, {
+    cors: {
+        origin: `http://localhost:${PORT}`,
+        methods: ["GET", "POST"],
+        transports: ["websocket", "polling"],
+        credentials: true
+    },
+    allowEIO3: true
+});
+
+app.use(express.static(path.resolve(__dirname, '../client/out/')));
+app.use('/api', wsRouter);
+app.set('socket.io', io);
